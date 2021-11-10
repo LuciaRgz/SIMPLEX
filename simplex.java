b3a6e13ed977759563634f900cf6ba22863be818
LuciaRgz <ly30246@gmail.com>
2021-11-10 13:47:15 -0600

    update: repo

---
+++ simplex.java
@@ -0,0 +1,8 @@
+//The next line is the library that we are going to use. :)
+import java.io.*;
+class Simplex {
+    public static void main (String args[]) throws IOException{
+        System.out.println("Welcome to Simplex Method");
+        
+    }
+}
